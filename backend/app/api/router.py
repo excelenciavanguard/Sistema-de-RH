5eae6b52a718c16a9d7192f4c2f4a9723fc51952
gabriel-vanguard <gabriel.vanguard@grexcelencia.com.br>
2026-09-15 16:34:03 -0300

    Delete backend directory

--- backend/app/api/router.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-
-from .routes import candidates, extractions, system
-
-
-api_router = APIRouter(prefix="/api/v1")
-api_router.include_router(system.router)
-api_router.include_router(extractions.router)
-api_router.include_router(candidates.router)
